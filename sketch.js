43a22da1d9b25868298817c9a971163237b660e2
disha2613 <82595224+disha2613@users.noreply.github.com>
2021-05-24 13:54:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,10 +11,10 @@ const Body = Matter.Body;
 
 function preload()
 {
-	starImg = loadImage("images/star.png");
-	bgImg = loadImage("images/starNight.png");
-	fairyImg = loadImage("images/fairyImage1.png","images/fairyImage2.png");
-	fairyVoice = loadSound("sound/JoyMusic.mp3");
+	starImg = loadImage("star.png");
+	bgImg = loadImage("starNight.png");
+	fairyImg = loadImage("fairyImage1.png","fairyImage2.png");
+	fairyVoice = loadSound("JoyMusic.mp3");
 	//load animation for fairy here
 }
 
@@ -25,7 +25,7 @@ function setup() {
 	fairyVoice.play();
 	//create fairy sprite and add animation for fairy
 	fairy = createSprite(130,520);
-	fairy.addAnimation("flyingfairy", fairyImg);
+	fairy.addAnimation("flyingfairy",fairyImg);
 	fairy.scale = 0.3;
 
 	star = createSprite(650,30);
